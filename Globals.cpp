512655b4481e9e4b8bcf3c1a5d09743ce5a5178a
andreja6 <andrejaandjic@hotmail.com>
2018-05-02 17:10:29 -0700

    Changed spacing

--- Globals.cpp
+++ Globals.cpp
@@ -6,11 +6,6 @@ int const Globals::major = 0;
 int const Globals::minor = 4;
 int const Globals::patch = 2;
 
-Globals::Globals(void)
-{
+Globals::Globals(void){}
 
-}
-
-Globals::~Globals(void)
-{
-}
+Globals::~Globals(void){}
