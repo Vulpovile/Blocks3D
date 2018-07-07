e7ea4c4b729b06f142129f7a6c1738ce651f9b3e
andreja6 <andrejaandjic@hotmail.com>
2018-07-07 12:46:28 -0700

    It compiles :+1:

--- IEDispatcher.cpp
+++ IEDispatcher.cpp
@@ -7,3 +7,34 @@ IEDispatcher::IEDispatcher(void)
 IEDispatcher::~IEDispatcher(void)
 {
 }
+
+HRESULT STDMETHODCALLTYPE IEDispatcher::QueryInterface(const IID &riid, void **ppvObject)
+{
+	return NULL;
+}
+ULONG STDMETHODCALLTYPE IEDispatcher::AddRef()
+{
+	return NULL;
+}
+ULONG STDMETHODCALLTYPE IEDispatcher::Release()
+{
+	return NULL;
+}
+HRESULT STDMETHODCALLTYPE IEDispatcher::GetTypeInfoCount(UINT *pctinfo)
+{
+	return NULL;
+}
+HRESULT STDMETHODCALLTYPE IEDispatcher::GetTypeInfo(UINT, LCID, ITypeInfo **)
+{
+	return NULL;
+}
+HRESULT STDMETHODCALLTYPE IEDispatcher::GetIDsOfNames(const IID &, LPOLESTR *, UINT, LCID, DISPID *)
+{
+	return NULL;
+}
+HRESULT STDMETHODCALLTYPE IEDispatcher::Invoke(DISPID, const IID &, LCID, WORD, DISPPARAMS *, VARIANT *, EXCEPINFO *, UINT *)
+
+{
+	return NULL;
+}
+	
