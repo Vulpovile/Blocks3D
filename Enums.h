d16bf9e39bd97753f7a0f0f7458accdd4a04216f
DirtPiper <oldrobloxguy@gmail.com>
2018-12-10 21:45:39 -0500

    Added Spawn to SurfaceType

--- Enums.h
+++ Enums.h
@@ -7,7 +7,7 @@ static enum ActionType {Nothing, Pause, Lose, Draw, Win};
 static enum AffectType {NoChange, Increase, Decrease};
 static enum InputType {NoInput, LeftTread, RightTread, Steer, Throtle, UpDown, Action1, Action2, Action3, Action4, Action5, Constant, Sin};
 //static enum SurfaceConstraint {None, Hinge, SteppingMotor, Motor};
-static enum SurfaceType{Smooth, Snaps, Inlets, Glue, Weld, Hinge, Motor, Bumps};
+static enum SurfaceType{Smooth, Snaps, Inlets, Glue, Weld, Spawn, Hinge, Motor, Bumps};
 static enum SoundType {NoSound, Boing, Bomb, Break, Click, Clock, Slingshot, Page, Ping, Snap, Splat, Step, StepOn, Swoosh, Victory};
 static enum PartType {Ball, Block, Cylinder};
 static enum KeywordFilterType {Include, Exclude};
