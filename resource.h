aab26d2e35d54745d9c1120b4b21cadd44f7852f
Vulpovile <andrejaandjic@hotmail.com>
2021-03-20 21:38:44 -0700

    Finally got VS2005 to shut the hell up

--- resource.h
+++ resource.h
@@ -1,7 +1,19 @@
-#define IDC_PROPERTYGRID                2000
-#ifndef IDC_STATIC
-#define IDC_STATIC (-1)
-#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by dialogs.rc
+//
+#define IDC_PROPERTYGRID				2000
 
-#define IDB_BITMAP1                             102
-#define IDI_ICON1                               107
+#define IDI_ICON1                       101
+#define IDB_BITMAP1                     102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
