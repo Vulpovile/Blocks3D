c760abe0ceedb0989c2d02e400ee112e70c0715d
pinksub <114634497+pinksub@users.noreply.github.com>
2022-10-22 20:08:15 -0400

    Remove weird title variables that are the exact same as g_appName
    There isn't a very good reason to make these variables, one good one would be to convert the types, but g_appName is the exact same type as title.

--- src/source/ErrorFunctions.cpp
+++ src/source/ErrorFunctions.cpp
@@ -6,8 +6,8 @@
 
 void OnError(int err, std::string msg)
 {
-	std::string emsg = "An unexpected error has occured and "+g_appName+" has to quit. We're sorry!" + msg;
-	std::string title = g_appName+" Crash";
+	std::string emsg = "An unexpected error has occured and " + g_appName + " has to quit. We're sorry!" + msg;
+	std::string title = g_appName + " Crash";
 	MessageBox(NULL, emsg.c_str(), title.c_str(), MB_OK);
 	exit(err);
 }
@@ -15,13 +15,11 @@ void OnError(int err, std::string msg)
 
 void MessageBoxStr(std::string msg)
 {
-	std::string title = g_appName;
-	MessageBox(NULL, msg.c_str(), title.c_str(), MB_OK);
+	MessageBox(NULL, msg.c_str(), g_appName.c_str(), MB_OK);
 }
 
 void MessageBoxStream(std::stringstream msg)
 {
 	std::string strMsg = msg.str();
-	std::string title = g_appName;
-	MessageBox(NULL, strMsg.c_str(), title.c_str(), MB_OK);
+	MessageBox(NULL, strMsg.c_str(), g_appName.c_str(), MB_OK);
 }
